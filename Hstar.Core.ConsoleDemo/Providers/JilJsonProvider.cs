0e032146d15040667be5651f29cbfebdad4f70a8
Jay.M.Hu <hm910705@163.com>
2015-02-04 14:29:55 +0800

    Add a json serialize library.

---
+++ Hstar.Core.ConsoleDemo/Providers/JilJsonProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hstar.Core.Serializer;
+
+namespace Hstar.Core.ConsoleDemo.Providers
+{
+    /// <summary>
+    /// Jil 需要.Net 4.5支持
+    /// </summary>
+    class JilJsonProvider:IJsonSerializerProvider
+    {
+        public string Serialize(object obj, string datetimeFormat = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Deserialize<T>(string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
