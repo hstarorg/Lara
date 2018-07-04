848660472ee2ff377a3eebe2b730cc86f56b1c5f
Hstarorg <hm910705@163.com>
2018-07-04 23:44:02 +0800

    Md5 helper done(with test).

--- src/Hstar.Lara.Utilities/Security/MD5Helper.cs
+++ src/Hstar.Lara.Utilities/Security/MD5Helper.cs
@@ -2,39 +2,32 @@
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
+using System.Linq;
 
 namespace Hstar.Lara.Utilities.Security
 {
     public static class MD5Helper
     {
-        public static string ComputeMD5Hash(this string str, Encoding encoding = null, Encoding outputEncoding = null)
+        public static string ComputeMD5Hash(this string str, Encoding encoding = null)
         {
             if (encoding == null)
             {
                 encoding = Encoding.UTF8;
-            }
-            if (outputEncoding == null)
-            {
-                outputEncoding = Encoding.UTF8;
             }
             var md5 = MD5.Create();
             var md5Bytes = md5.ComputeHash(encoding.GetBytes(str));
-            return outputEncoding.GetString(md5Bytes);
+            return string.Join("", md5Bytes.Select(x => x.ToString("x2")));
         }
 
-        public static string ComputeHMACMD5Hash(this string str, Encoding encoding = null, Encoding outputEncoding = null)
+        public static string ComputeHMACMD5Hash(this string str, string key, Encoding encoding = null)
         {
             if (encoding == null)
             {
                 encoding = Encoding.UTF8;
             }
-            if (outputEncoding == null)
-            {
-                outputEncoding = Encoding.UTF8;
-            }
-            var hmacMD5 = HMACMD5.Create();
+            var hmacMD5 = new HMACMD5(encoding.GetBytes(key));
             var md5Bytes = hmacMD5.ComputeHash(encoding.GetBytes(str));
-            return outputEncoding.GetString(md5Bytes);
+            return string.Join("", md5Bytes.Select(x => x.ToString("x2")));
         }
     }
 }
